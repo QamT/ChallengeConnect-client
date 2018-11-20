88fbdd7266692ad90c4abba2240054f66a23dbef
Qamar Idrees <qam.idrees@gmail.com>
2018-11-20 13:57:32 -0600

    initial commit

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => (
+  <div>
+      <h1>Profile</h1>
+      <p>Profile set up</p>
+  </div>
+)
---
+++ src/pages/Register/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => (
+  <div>
+      <h1>Sign Up</h1>
+      <p>Form</p>
+  </div>
+)
